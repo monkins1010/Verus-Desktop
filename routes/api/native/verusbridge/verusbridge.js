1ac0a0f337c2119fd1c5daf92e7021f891ed7b5e
Chris <monkins1010@gmail.com>
2022-05-25 11:35:09 +0100

    updates

--- routes/api/native/verusbridge/verusbridge.js
+++ routes/api/native/verusbridge/verusbridge.js
@@ -2,28 +2,39 @@ const server = require("verus_bridgekeeper");
 
 module.exports = (api) => { 
   api.native.start_bridgekeeper = () => {
-
-    try {
-      server.alanStart();
-
-    } catch (e) {
-      throw new Error("Bridge Keeper failed to start")
-    }
-
+    return new Promise((resolve, reject) => {
+      const result = server.start()
+      
+      if(result === true)
+       resolve(result)
+      else
+        reject(result)
+    })
   };
 
   api.native.stop_bridgekeeper = () => {
-
-    try {
-      server.alanStop();
-
-    } catch (e) {
-      throw new Error("Bridge Keeper failed to stop")
-    }
-
+    return new Promise((resolve, reject) => {
+      const result = server.stop()
+      
+      if(result === true)
+       resolve(result)
+      else
+        reject(result)
+    })
   };
 
-  api.setGet('/native/start_bridgekeeper', (req, res, next) => {    
+  api.native.bridgekeeper_status = () => {
+    return new Promise((resolve, reject) => {
+      const result = server.status()
+      
+      if(result)
+       resolve(result)
+      else
+        reject(result)
+    })
+  };
+
+  api.setPost('/native/start_bridgekeeper', (req, res, next) => {    
     api.native.start_bridgekeeper()
     .then((reply) => {
       const retObj = {
@@ -43,8 +54,28 @@ module.exports = (api) => {
     })
   });
 
-  api.setGet('/native/stop_bridgekeeper', (req, res, next) => {    
+  api.setPost('/native/stop_bridgekeeper', (req, res, next) => {    
     api.native.stop_bridgekeeper()
+    .then((reply) => {
+      const retObj = {
+        msg: 'success',
+        result: reply,
+      };
+  
+      res.send(JSON.stringify(retObj));  
+    })
+    .catch(error => {
+      const retObj = {
+        msg: 'error',
+        result: error.message,
+      };
+  
+      res.send(JSON.stringify(retObj));  
+    })
+  });
+
+  api.setPost('/native/bridgekeeper_status', (req, res, next) => {    
+    api.native.bridgekeeper_status()
     .then((reply) => {
       const retObj = {
         msg: 'success',
