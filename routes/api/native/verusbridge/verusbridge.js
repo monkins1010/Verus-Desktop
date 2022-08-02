8bebc00fb68db2ef75596bbc017573e45599de76
michaeltout <toutonghi.michael@gmail.com>
2022-08-02 11:59:50 -0700

    Merge branch 'bridgekeeper' of https://github.com/monkins1010/Verus-Desktop into dev

---
+++ routes/api/native/verusbridge/verusbridge.js
@@ -0,0 +1,139 @@
+const server = require("verus_bridgekeeper");
+
+module.exports = (api) => { 
+
+  api.native.start_bridgekeeper = () => {
+    return new Promise((resolve, reject) => {
+
+      const setupConf = api.native.loadVethConfig();
+
+      if (setupConf !== true) {
+        reject(setupConf);
+      } else {
+        server.start().then((result) => {
+          if(result === true)
+          resolve(result)
+          else
+            reject(result)
+        })
+      }
+    })
+  };
+
+  api.native.stop_bridgekeeper = () => {
+    return new Promise((resolve, reject) => {
+      const result = server.stop()
+      
+      if(result === true)
+       resolve(result)
+      else
+        reject(result)
+    })
+  };
+
+  api.native.bridgekeeper_status = () => {
+    return new Promise((resolve, reject) => {
+      const result = server.status()
+      
+      if(result)
+       resolve(result)
+      else
+        reject(result)
+    })
+  }; 
+
+  api.native.bridgekeeper_setconf = (key, infuraLink, ethContract) => {
+    return new Promise((resolve, reject) => {
+      const result = server.set_conf(key, infuraLink, ethContract)
+      
+      if(result)
+       resolve(result)
+      else
+        reject(result)
+    })
+  };
+
+  api.setPost('/native/start_bridgekeeper', (req, res, next) => {    
+    api.native.start_bridgekeeper()
+    .then((reply) => {
+      const retObj = {
+        msg: 'success',
+        result: reply,
+      };
+  
+      res.send(JSON.stringify(retObj));  
+    })
+    .catch(error => {
+      const retObj = {
+        msg: 'error',
+        result: error.message,
+      };
+  
+      res.send(JSON.stringify(retObj));  
+    })
+  });
+
+  api.setPost('/native/stop_bridgekeeper', (req, res, next) => {    
+    api.native.stop_bridgekeeper()
+    .then((reply) => {
+      const retObj = {
+        msg: 'BridgeKeeper Stopped',
+        result: reply,
+      };
+  
+      res.send(JSON.stringify(retObj));  
+    })
+    .catch(error => {
+      const retObj = {
+        msg: 'error',
+        result: error.message,
+      };
+  
+      res.send(JSON.stringify(retObj));  
+    })
+  });
+
+  api.setPost('/native/bridgekeeper_status', (req, res, next) => {    
+    api.native.bridgekeeper_status()
+    .then((reply) => {
+      const retObj = {
+        msg: 'success',
+        result: reply,
+      };
+  
+      res.send(JSON.stringify(retObj));  
+    })
+    .catch(error => {
+      const retObj = {
+        msg: 'error',
+        result: error.message,
+      };
+  
+      res.send(JSON.stringify(retObj));  
+    })
+  }); 
+
+  api.setPost('/native/bridgekeeper_setconf', (req, res, next) => {   
+    
+    const { key, infuraLink, ethContract } = req.body;
+    api.native.bridgekeeper_setconf(key, infuraLink, ethContract)
+    .then((reply) => {
+      const retObj = {
+        msg: 'success',
+        result: reply,
+      };
+  
+      res.send(JSON.stringify(retObj));  
+    })
+    .catch(error => {
+      const retObj = {
+        msg: 'error',
+        result: error
+      };
+  
+      res.send(JSON.stringify(retObj));  
+    })
+  }); 
+
+  return api;
+};
---
+++ routes/api/native/verusbridge/vethconf.js
@@ -0,0 +1,149 @@
+const fs = require('fs-extra');
+const os = require('os');
+const path = require('path');
+var ini = require('ini');
+const fixPath = require('fix-path');
+
+const INIKeys = {
+    rpcuser: '',
+    rpcpassword: '',
+    rpcport: '',
+    rpchost: '',
+    upgrademanageraddress: '',
+    testnet: '',
+    privatekey: '',
+    ethnode: '',
+};
+
+const pbaasRootOptions = {
+    VRSCTEST: {
+        darwin: "/VerusTest",
+        linux: "/.verustest",
+        win32: "/VerusTest"
+    },
+    VRSC: {
+        darwin: "/Verus",
+        linux: "/.verus",
+        win32: "/Verus"
+    },
+};
+
+const vethFolder = {
+    "000b090bec6c9ff28586eb7ed24e77562f0c4667": {
+        darwin: "/pbaas/000b090bec6c9ff28586eb7ed24e77562f0c4667",
+        linux: "/pbaas/000b090bec6c9ff28586eb7ed24e77562f0c4667",
+        win32: "/pbaas/000b090bec6c9ff28586eb7ed24e77562f0c4667"
+    },
+};
+
+const RPCDefault = {
+    VRSCTEST: {
+        rpcuser: "user",
+        rpcpassword: "password",
+        rpcport: 8000,
+        rpchost: "127.0.0.1",
+        upgrademanageraddress: "empty",
+        testnet: true,
+        privatekey: "empty",
+        ethnode: "empty",
+    },
+    VRSC: {
+        rpcuser: "username",
+        rpcpassword: "password",
+        rpcport: 8000,
+        rpchost: "127.0.0.1",
+        upgrademanageraddress: "empty",
+        testnet: false,
+        privatekey: "empty",
+        ethnode: "wss://rinkeby.infura.io/ws/v3/........",
+    }
+}
+
+module.exports = (api) => {
+
+  api.native.loadVethConfig = () => {
+
+    const VETH = "000b090bec6c9ff28586eb7ed24e77562f0c4667";
+
+    let chaintc = "VRSCTEST"; //chainName.toUpperCase();
+    
+    let Config = INIKeys;
+    const pbaasFolder = vethFolder[VETH]; 
+    const pbaasRoot = pbaasRootOptions[chaintc];
+
+    let confPath;
+
+    switch (os.platform()) {
+        case 'darwin':
+            fixPath();
+            confPath = `${global.HOME}/Library/Application Support` +  pbaasRoot.darwin + pbaasFolder.darwin;
+            break;
+        case 'win32':
+            confPath = `${global.HOME}` + pbaasRoot.win32 + pbaasFolder.win32;
+            confPath = path.normalize(confPath);
+            break;
+        case 'linux':
+            confPath = `${global.HOME}` + pbaasRoot.linux + pbaasFolder.linux;
+            break;
+    }
+
+    if (!fs.existsSync(confPath)) {
+        fs.mkdirSync(confPath, { recursive: true });
+    }
+
+    let _data = {};
+    try {
+        _data = fs.readFileSync(confPath + '/' + VETH + '.conf', 'utf8');
+    } catch (error) {
+        if (error.code != 'ENOENT') {
+            console.log("Error reading file at: ", confPath + "\nError: " + error.message);
+            return new Error("Error reading file at: ", confPath + "\nError: " + error.message)
+        }
+    }
+
+    if (_data.length && fs.existsSync(confPath + '/' + VETH + '.conf')) {
+        let _match;
+
+        console.log("(veth.conf) file found at: ", confPath);
+        for (const [key, value] of Object.entries(Config)) {
+
+            if (_match = _data.match(`${key}` + '=\\n*(.*)')) {
+
+                if (_match[1] != "empty") {
+                    Config[key] = _match[1];
+                } else {
+                    console.log("Empty veth.conf file value: ", `${key}:"empty" `);
+                    return new Error("Empty veth.conf file value: " + `${key}:"empty" `)
+                }
+            }
+        }
+        
+    } else {
+
+        try {
+
+        fs.writeFileSync(confPath + '/' + VETH + '.conf', "", 'utf8');
+        }
+        catch (e) {
+
+            return new Error('Error writing veth.conf: ', e.message);
+
+        }    
+
+        for (const [key, value] of Object.entries(RPCDefault[chaintc])) {
+            fs.appendFileSync(confPath + '/' + VETH + '.conf', `${key}=${value}` + "\n");
+        }
+
+        let tempvalues = fs.readFileSync(confPath + '/' + VETH + '.conf', 'utf8');
+
+        const errorMsg = "Please check veth.conf file located at: " + path.normalize(confPath + '/' + VETH + '.conf') +
+         "/n" + "Default Values:\n" + JSON.stringify(ini.parse(tempvalues, 'utf-8'))
+        console.log(errorMsg);
+        return new Error("Please complete the Bridge setup using the gear icon");
+    }
+    return true;
+
+  }
+
+  return api;
+};
